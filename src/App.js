5db6df26005c9686ed9678f0f83499bebe41bfee
flylex2 <volodymirmail@gmail.com>
2023-08-03 15:03:02 +0300

    users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
-import { Users } from './components/Users';
+import { Users } from './components/Users/index';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [invites, setInvites] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+  
+  React.useEffect(() =>{
+    fetch("https://reqres.in/api/users")
+    .then(res=> res.json())
+    .then((json) => {
+      setUsers(json.data);
+      //console.log('Вывод дата = ',json.data);
+    }).catch((err) =>{
+      console.warn(err);
+      alert('Ошибка');
+    })
+    .finally(setLoading(false));
+  },[]);
+
+  const onChageSetSearchValue =( event) =>{
+    setSearchValue(event.target.value)
+  }
+  const onClickInvite = (id) =>{
+      if(invites.includes(id)){
+        setInvites(prev => prev.filter(_id => _id !== id) );
+      }else {
+        setInvites(prev => [...prev,id]);
+      }
+  }
+  const onClickSendInvites = () =>{
+    setSuccess(true);
+  }
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+     {
+      success ? (
+        <Success count={invites.length} /> 
+    ) : ( 
+      <Users  
+      onChageSetSearchValue={onChageSetSearchValue}
+      searchValue={searchValue}
+      items={users} 
+      isLoading={isLoading}
+      invites={invites}
+      onClickInvite={onClickInvite} 
+      onClickSendInvites={onClickSendInvites}
+      />
+    )} 
     </div>
   );
 }
 
 export default App;
+
